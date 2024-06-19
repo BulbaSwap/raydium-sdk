7ec0cc97c396755e602ace848be66decee6957f0
Clayon <admin@github.xyz>
2024-06-19 23:53:58 +0800

    feat: update

--- src/utils/createMarket.ts
+++ src/utils/createMarket.ts
@@ -204,6 +204,12 @@ export class MarketV2 extends Base {
         space: MARKET_STATE_LAYOUT_V2.span,
         programId: marketInfo.programId,
       }),
+      createInitializeAccountInstruction(marketInfo.baseVault.publicKey, marketInfo.baseMint, marketInfo.vaultOwner),
+      createInitializeAccountInstruction(marketInfo.quoteVault.publicKey, marketInfo.quoteMint, marketInfo.vaultOwner),
+    )
+
+    const ins2: TransactionInstruction[] = []
+    ins2.push(
       SystemProgram.createAccountWithSeed({
         fromPubkey: wallet,
         basePubkey: wallet,
@@ -222,12 +228,6 @@ export class MarketV2 extends Base {
         space: 11308,
         programId: marketInfo.programId,
       }),
-      createInitializeAccountInstruction(marketInfo.baseVault.publicKey, marketInfo.baseMint, marketInfo.vaultOwner),
-      createInitializeAccountInstruction(marketInfo.quoteVault.publicKey, marketInfo.quoteMint, marketInfo.vaultOwner),
-    )
-
-    const ins2: TransactionInstruction[] = []
-    ins2.push(
       SystemProgram.createAccountWithSeed({
         fromPubkey: wallet,
         basePubkey: wallet,
@@ -288,8 +288,6 @@ export class MarketV2 extends Base {
             InstructionType.createAccount,
             InstructionType.createAccount,
             InstructionType.createAccount,
-            InstructionType.createAccount,
-            InstructionType.createAccount,
             InstructionType.initAccount,
             InstructionType.initAccount,
           ],
@@ -298,6 +296,8 @@ export class MarketV2 extends Base {
           instructions: ins2,
           signers: [],
           instructionTypes: [
+            InstructionType.createAccount,
+            InstructionType.createAccount,
             InstructionType.createAccount,
             InstructionType.createAccount,
             InstructionType.initMarket,
